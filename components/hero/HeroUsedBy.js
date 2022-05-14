96e47e6bda0cf34f65ff63fe4a3f28291f3a9d54
Hritwik Agarwal <39455725+HR199812@users.noreply.github.com>
2022-05-14 15:11:19 +0530

    Update HeroUsedBy.js

--- components/hero/HeroUsedBy.js
+++ components/hero/HeroUsedBy.js
@@ -1,8 +1,19 @@
-const HeroUsedBy = () =>{
-    return(
+const HeroUsedBy = () => {
+  return (
     <>
-    <div></div>
+      <div className="hero-usedBy">
+        <div className="hero-usedBy-copy">
+          <p>
+            The tappable story format has never been more accessible—to creators
+            and readers alike. See what happens when Google brings stories to
+            the open web.
+          </p>
+        </div>
+        <div className="hero-usedBy-logos">
+          <p>Making an impact at</p>
+        </div>
+      </div>
     </>
-    )
-}
+  );
+};
 export default HeroUsedBy;
